44dee84458b1414cdb3b301598af2223f0c72458
Adamu-Clement <adamuclementjatau@gmail.com>
2022-06-07 00:31:30 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+/*
+ * File: 0-putchar.c
+ * Auth: Clement Adamu 
+ *
+ */
+#include "holberton.h"
+/**
+ * main - Prints "ClementAJ" followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_putchar('C');
+	_putchar('l');
+	_putchar('e');
+	_putchar('m');
+	_putchar('e');
+	_putchar('n');
+	_putchar('t');
+	_putchar('A');
+	_putchar('J');
+	_putchar('\n');
+
+	return (0);
+}
