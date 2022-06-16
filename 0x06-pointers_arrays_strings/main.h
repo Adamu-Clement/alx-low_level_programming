64d2987d3ea010121b685fc9f8b24d26670c2fd3
Adamu-Clement <adamuclementjatau@gmail.com>
2022-06-16 07:11:34 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -8,4 +8,6 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 int _putchar(char c);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 #define NULL 0
