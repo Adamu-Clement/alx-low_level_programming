8550757e122dbebfd4c811b39b36c5b19ff5e154
Adamu-Clement <adamuclementjatau@gmail.com>
2022-06-25 06:14:08 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 /**
- *Author: ADAMU Clement 
- *File   : 1-arg.c
  * main - prints the number of arguments passed into it.
  * @argc: number of command line arguments.
  * @argv: array that contains the program command line arguments.
