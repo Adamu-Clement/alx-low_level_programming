6df8b34fa238a421c0f6cd5f1a853d05b35f2c70
Adamu-Clement <adamuclementjatau@gmail.com>
2022-09-06 12:36:17 -0700

    100-operations.so

---
+++ 0x18-dynamic_libraries/isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*Author : ADAMU Clement
+* File  : 1-isdigit.c
+* _isdigit - function that verifies if a  character is a digit or not
+*@c: tested character
+*Return: returns 1 if it is uppercase , 0 if not
+*/
+
+int _isdigit(int c)
+{
+	if ((c >= 48) && (c <= 57))
+		return (1);
+	else
+		
+		return (0);
+}
